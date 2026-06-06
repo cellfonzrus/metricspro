ebeb7247d3e6f357ccf4f81177552886c5d824e6
cellfonzrus <59049533+cellfonzrus@users.noreply.github.com>
2026-06-06 18:38:28 +0000

    fix: allow all CORS origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://metricspro.vercel.app", "https://metricspro.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
